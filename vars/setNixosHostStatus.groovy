b5eba8386d00b28083bbe89e0ceb99b0971166cd
Mia Moir <me@archess.mn>
2025-12-29 23:00:13 +0000

    Change back to named variables

--- vars/setNixosHostStatus.groovy
+++ vars/setNixosHostStatus.groovy
@@ -1,3 +1,3 @@
-def set(String host, String status) {
+def call(String host, String status) {
     hostStatus = sh script: "consul kv put 'jenkins/NixOS/hosts/${host}/status' ${status}", returnStdout: true
 }
