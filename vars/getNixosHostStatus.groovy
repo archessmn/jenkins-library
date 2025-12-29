548abba8c405cd2e179761526ec8c7b6dd52c8fe
Mia Moir <me@archess.mn>
2025-12-29 22:34:00 +0000

    Revert "Change to get/set files"
    This reverts commit 2c8297f43b0694dabca3b27f6e9fe9aa947d45fe.

--- vars/getNixosHostStatus.groovy
+++
@@ -1,4 +0,0 @@
-def call(String host) {
-    hostStatus = sh script: "consul kv get 'jenkins/NixOS/hosts/${host}/status' || true", returnStdout: true
-    return hostStatus
-}
--- vars/setNixosHostStatus.groovy
+++
@@ -1,3 +0,0 @@
-def set(String host, String status) {
-    hostStatus = sh script: "consul kv put 'jenkins/NixOS/hosts/${host}/status' ${status}", returnStdout: true
-}
