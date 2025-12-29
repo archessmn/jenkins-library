2f7344fcbe9c97e3122dd5db771bf058b8d8a4a9
Mia Moir <me@archess.mn>
2025-12-29 22:50:16 +0000

    Try with LinkedHashMap

--- vars/nixosHostStatus.groovy
+++ vars/nixosHostStatus.groovy
@@ -1,8 +1,8 @@
-def get(Map params) {
+def get(LinkedHashMap params) {
     hostStatus = sh script: "consul kv get 'jenkins/NixOS/hosts/${params.host}/status' || true", returnStdout: true
     return hostStatus
 }
 
-def set(Map params) {
+def set(LinkedHashMap params) {
     hostStatus = sh script: "consul kv put 'jenkins/NixOS/hosts/${params.host}/status' ${params.status}", returnStdout: true
 }
